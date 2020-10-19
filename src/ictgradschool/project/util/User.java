c9e2cc54036f3bea781af8acf292db50e34c31ea
gwar791 <gwar791@aucklanduni.ac.nz>
2020-10-19 22:49:56 +1300

    I created a Stylesheet for the project and edited the index.jsp file to make a basic example of how the website landing page could look

---
+++ src/ictgradschool/project/util/User.java
@@ -0,0 +1,11 @@
+package ictgradschool.project.util;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    String fName;
+    String lName;
+    int age;
+    String description;
+
+}
